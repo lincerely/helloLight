485e216d124e1a2cd41b77f31d73c2635e4de256
lincerely <lincerely@gmail.com>
2019-09-17 02:02:54 +0800

    add reflection

--- helloLight.lua
+++ helloLight.lua
@@ -11,9 +11,10 @@ lx=120
 ly=68
 lf=4628
 lr=10
+ls=1
 
 -- global illumination (0-8)
-gi=10
+gi=0
 
 lastTime=0
 shades={}
@@ -23,7 +24,7 @@ function loadShades()
 		shades[color]={}
 
 		local shadeSpr=0
-		if color<8 then shadeSpr=0 else shadeSpr=16 end
+		if color<8 then shadeSpr=6 else shadeSpr=22 end
 		local sprAddr=0x4000+32*shadeSpr
 
 		for amount=0,7 do		
@@ -35,57 +36,86 @@ end
 
 function init()
 	loadShades()
+	local w,h= 160,144
+	clip((240-w)/2,0,w,h)
 end
 
 init()
 function TIC()
-	if btn(0) then ly=ly-2 end
-	if btn(1) then ly=ly+2 end
-	if btn(2) then lx=lx-2 end
-	if btn(3) then lx=lx+2 end
+	if btn(0) then y=y-2 end
+	if btn(1) then y=y+2 end
+	if btn(2) then x=x-2 end
+	if btn(3) then x=x+2 end
 
-	if btn(4) then lr=lr+1 end
-	if btn(5) then lr=lr-1 end
+	if btn(4) then gi=gi+1 end
+	if btn(5) and gi>1 then gi=gi-1 end
 
 	cls(13)
 	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
+	print("HELLO WORLD!",84,70)
 	t=t+1
 
 	local now=time()
 	local fps=1000/(now-lastTime)
-	print("FPS:"..math.floor(fps),0,0,6)
+	print("FPS:"..math.floor(fps),90,100,6)
 	lastTime=now
+
+	ly = y + 20
+	lx = x + 50
+	lr = 8 + math.random(1)
+
+	drawShadow()
+	drawWater()
 end
 
 function sqrDist( x1, y1, x2, y2 )
 	return (x2-x1)^2 + (y2-y1)^2
 end
 
+waterY = 20
+function drawWater()
+	local addrW = (136 - waterY) * 120 
+	local addrR = (136 - waterY - 1) * 120
+	for yOffset=1,waterY do
+		memcpy(addrW, addrR, 120)
+		addrW=addrW+120
+		addrR=addrR-120
+	end
+end
 
 function SCN(scnline)
-	for i=0,238,2 do
-		local r1=1/sqrDist(lx,ly,i,scnline)
-		local shadeAmount1= (lr - r1*lf) // 1
-		shadeAmount1=math.min(shadeAmount1,7)
-		shadeAmount1=math.max(shadeAmount1,0)
+end
 
-		local r2=1/sqrDist(lx,ly,i+1,scnline)
-		local shadeAmount2= (lr - r2*lf) // 1
-		shadeAmount2=math.min(shadeAmount2,7)
-		shadeAmount2=math.max(shadeAmount2,0)
+function drawShadow()
+	for scnline=0,136 do
+		if scnline > 136 - waterY then return end
 
-		if shadeAmount1~=0 or shadeAmount2~=0 then
-			local orgColors=peek(scnline*120+i/2)
+		for i=0,238,2 do
+			lf = 4478 + math.random(100)
+			local r1=1/sqrDist(lx,ly,i,scnline) * ls
+			local shadeAmount1= (lr - r1*lf) // 1
+			shadeAmount1=shadeAmount1-gi
+			shadeAmount1=math.min(shadeAmount1,7)
+			shadeAmount1=math.max(shadeAmount1,0)
 
-			local color1=orgColors&15
-			local color2=(orgColors>>4)&15
+			local r2=1/sqrDist(lx,ly,i+1,scnline) * ls
+			local shadeAmount2= (lr - r2*lf) // 1
+			shadeAmount2=shadeAmount2-gi
+			shadeAmount2=math.min(shadeAmount2,7)
+			shadeAmount2=math.max(shadeAmount2,0)
 
-			local shade1=shades[color1][shadeAmount1] 
-			local shade2=shades[color2][shadeAmount2] 
+			if shadeAmount1~=0 or shadeAmount2~=0 then
+				local orgColors=peek(scnline*120+i/2)
 
-			local finalColors=shade2<<4 | shade1
-			poke(scnline*120+i/2,finalColors)
+				local color1=orgColors&15
+				local color2=(orgColors>>4)&15
+
+				local shade1=shades[color1][shadeAmount1] 
+				local shade2=shades[color2][shadeAmount2] 
+
+				local finalColors=shade2<<4 | shade1
+				poke(scnline*120+i/2,finalColors)
+			end
 		end
 	end
 end
