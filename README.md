eaa81a744e2b233c9e5f24784d1a329ff2339da5
lincerely <lincerely@gmail.com>
2020-03-24 19:31:41 +0800

    refactor for readability

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# Light demo for TIC-80
+# Lighting demo for TIC-80
 
 Reference: [PICO-8 lighting series](https://hackernoon.com/pico-8-lighting-part-1-thin-dark-line-8ea15d21fed7)
 
 ![cover](doc/demo.gif)
 
 ## TODO
-
  - wave
  - character with walk cycle
  - under the water tunnel
  - splash water when walk
-
-
--- helloLight.lua
+++ helloLight.lua
@@ -7,11 +7,12 @@ t=0
 x=96
 y=24
 
+-- point light
 lx=120
 ly=68
-lf=4628
-lr=10
-ls=1
+lf=4478 -- fade
+lr=8 	-- radius
+ls=1 	-- strength
 
 -- global illumination (0-8)
 gi=0
@@ -36,71 +37,62 @@ end
 
 function init()
 	loadShades()
-	local w,h= 160,144
+	local w,h= 136,136
 	clip((240-w)/2,0,w,h)
 end
-
 init()
+
 function TIC()
+	local now=time()
+	local fps=1000/(now-lastTime)
+	lastTime=now
+
 	if btn(0) then y=y-2 end
 	if btn(1) then y=y+2 end
 	if btn(2) then x=x-2 end
 	if btn(3) then x=x+2 end
 
-	if btn(4) then gi=gi+1 end
+	ly=y+20
+	lx=x+50
+
+	if btn(4) and gi<8 then gi=gi+1 end
 	if btn(5) and gi>1 then gi=gi-1 end
 
 	cls(0)
+
 	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
 	print("HELLO WORLD!",84,70)
 	t=t+1
 
-	local now=time()
-	local fps=1000/(now-lastTime)
-	print("FPS:"..math.floor(fps),90,100,6)
-	lastTime=now
-
-	ly = y + 20
-	lx = x + 50
-	lr = 8 + math.random(1)
-
 	drawShadow()
 	drawWater()
+
+	print("GI:" .. gi, 54, 2)
+	print("FPS:"..math.floor(fps), 54, 12, 6)
 end
 
 function sqrDist( x1, y1, x2, y2 )
 	return (x2-x1)^2 + (y2-y1)^2
 end
 
-waterY = 20
-function drawWater()
-	local addrW = (136 - waterY) * 120 
-	local addrR = (136 - waterY - 1) * 120
-	for yOffset=1,waterY do
-		-- -1, 0, 1, 0, -1, 0, 1
-		memcpy(addrW, addrR + (yOffset+t//10)%2 - 1, 120)
-		addrW=addrW+120
-		addrR=addrR-120
-	end
-end
-
 function SCN(scnline)
 end
 
 function drawShadow()
+	lr_m=lr+math.random(1)
 	for scnline=0,136 do
 		if scnline > 136 - waterY then return end
 
 		for i=0,238,2 do
-			lf = 4478 + math.random(100)
+			lf_m = lf + math.random(100)
 			local r1=1/sqrDist(lx,ly,i,scnline) * ls
-			local shadeAmount1= (lr - r1*lf) // 1
+			local shadeAmount1= (lr_m - r1*lf_m) // 1
 			shadeAmount1=shadeAmount1-gi
 			shadeAmount1=math.min(shadeAmount1,7)
 			shadeAmount1=math.max(shadeAmount1,0)
 
 			local r2=1/sqrDist(lx,ly,i+1,scnline) * ls
-			local shadeAmount2= (lr - r2*lf) // 1
+			local shadeAmount2= (lr_m - r2*lf_m) // 1
 			shadeAmount2=shadeAmount2-gi
 			shadeAmount2=math.min(shadeAmount2,7)
 			shadeAmount2=math.max(shadeAmount2,0)
@@ -121,3 +113,14 @@ function drawShadow()
 	end
 end
 
+waterY = 20
+function drawWater()
+	local addrW = (136 - waterY) * 120 
+	local addrR = (136 - waterY - 1) * 120
+	for yOffset=1,waterY do
+		-- -1, 0, 1, 0, -1, 0, 1
+		memcpy(addrW, addrR + (yOffset+t//10)%2 - 1, 120)
+		addrW=addrW+120
+		addrR=addrR-120
+	end
+end
